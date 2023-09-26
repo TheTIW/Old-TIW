1fca4956bd2bdffeab1244b90df8ddb838c9a3e1
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-09-26 09:52:21 -0500

    Create gscript.js

---
+++ gscript.js
@@ -0,0 +1,37 @@
+function goBack() {
+    history.back();
+  }
+  
+  function reloadPage() {
+    location.reload();
+  }
+  
+  function toggleFullscreen() {
+    const iframe = document.getElementById('iframe');
+    if (iframe.requestFullscreen) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        iframe.requestFullscreen();
+      }
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+      if (document.mozFullScreenElement) {
+        document.mozCancelFullScreen();
+      } else {
+        iframe.mozRequestFullScreen();
+      }
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      if (document.webkitFullscreenElement) {
+        document.webkitExitFullscreen();
+      } else {
+        iframe.webkitRequestFullscreen();
+      }
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+      if (document.msFullscreenElement) {
+        document.msExitFullscreen();
+      } else {
+        iframe.msRequestFullscreen();
+      }
+    }
+  }
+  
